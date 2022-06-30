59823fbeb052a78a5191e6678180bc27175a62ae
Michel philippe Barutel <mbarutel@c3r3p1.hive.fi>
2022-06-30 17:51:59 +0300

    update

--- srcs/ft_ls.c
+++ srcs/ft_ls.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ls.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 11:21:27 by mbarutel          #+#    #+#             */
-/*   Updated: 2022/06/30 12:30:39 by mbarutel         ###   ########.fr       */
+/*   Updated: 2022/06/30 17:50:46 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 char    *file_type(mode_t mode)
 {
-    ft_printf("%i\n", mode);
     if (S_ISREG(mode))
         return ("File");
     if (S_ISDIR(mode))
@@ -32,11 +31,46 @@ char    *file_type(mode_t mode)
     return (NULL);
 }
 
+char    *permission_str(mode_t mode)
+{
+    int     len;
+    int     octal;
+    char    *octal_str;
+    int     perm_int;
+    char    *permission;
+
+    len = 8;
+    permission = ft_strdup("rwxrwxrwx");
+    octal_str = ft_itoa_base(mode, 8);
+    if (!permission || !octal_str)
+        return (NULL);
+    octal = ft_atoi(octal_str);
+    while (len > 0)
+    {
+        perm_int = octal % 10;
+        while (perm_int)
+        {
+            if ((perm_int % 2) == 0)
+                permission[len] = '-';
+            perm_int /= 2;
+            len--;
+        }
+        octal /= 10;
+    }
+    free(octal_str);
+    return (permission);
+}
+
 void    attr_printer(struct dirent *dent, struct stat *stat)
 {
-    printf("Name: %s\n", dent->d_name);
-    printf("Type: %s\n", file_type(stat->st_mode));
-    printf("\n");
+    char    *perm;
+    
+    ft_printf("Name            : %s\n", dent->d_name);
+    ft_printf("Type            : %s\n", file_type(stat->st_mode));
+    perm = permission_str(stat->st_mode);
+    ft_printf("Permissions     : %s\n", perm);
+    free(perm);
+    ft_printf("\n");
 }
 
 char    *get_path(char *input, char *file_name)
@@ -60,7 +94,6 @@ int main(int ac, char **av)
    
     if (ac > 1)
     {
-        // printf("get_path %s\n", get_path(av[1]));
         strcpy(buffer, av[1]);
         dir = opendir(buffer); // This invokes dir to point to the directory named within buffer
         if (dir)
@@ -71,9 +104,10 @@ int main(int ac, char **av)
                 buff = malloc(sizeof(struct stat));
                 if (!buff)
                     return (0);
-                // printf("get_path %s\n", get_path(av[1], dent->d_name));
-                stat(get_path(av[1], dent->d_name), buff);
+                path = get_path(av[1], dent->d_name);
+                lstat(path, buff);
                 attr_printer(dent, buff);
+                free(path);
                 free(buff);
                 dent = readdir(dir); // Calling this function within the while loop move the dir pointer.
             }
@@ -82,34 +116,3 @@ int main(int ac, char **av)
     }
     return (0);
 }
-
-// void    init_struct(s_file  *file_attr)
-// {
-//     file_attr->name = NULL;
-//     file_attr->type = NULL;
-// }
-
-// int main(int ac, char **av)
-// {
-//     // s_file          file_attr[100];
-//     DIR             *dir;
-//     char            buffer[50];
-//     struct dirent   *dent;
-   
-//     if (ac > 1)
-//     {
-//         strcpy(buffer, av[1]);
-//         dir = opendir(buffer); // This invokes dir to point to the directory named within buffer
-//         if (dir)
-//         {
-//             dent = readdir(dir); // readdir extracts information of the dir and puts it into dent which is a dirent struct
-//             while(dent) // Iterates over the directory and when it reaches the end, it will be NULL
-//             {
-//                 attr_printer(dent);
-//                 dent = readdir(dir); // Calling this function within the while loop move the dir pointer.
-//             }
-//             closedir(dir);
-//         }
-//     }
-//     return (0);
-// }
