19d9c72d9fe7387cdb13fd9eb0a5c609bad01b98
MrMikey695 <mikeedubas@hotmail.com>
2022-07-26 21:02:43 +0300

    fixed problem with bitfield permissions. Need testing

--- srcs/utils_one.c
+++ srcs/utils_one.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_one.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 11:21:27 by mbarutel          #+#    #+#             */
-/*   Updated: 2022/07/26 14:58:53 by mbarutel         ###   ########.fr       */
+/*   Updated: 2022/07/26 21:01:07 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -155,33 +155,115 @@ static int ft_pow(int nbr)
 }
 
 
-static char **permission_array(mode_t mode)
-{
-	int		index;
-	char	**permission;
-	char	*octal_str;
+// static char **permission_array(mode_t mode)
+// {
+// 	int		index;
+// 	int		pow_of;
+// 	int		index_1;
+// 	int		perm_val;
+// 	char	**permission;
+// 	char	*octal_str;
+// 	char	*tmp;
 	
 
-	index = 0;
-	octal_str = NULL;
-	permission = NULL;
-	permission = (char **)malloc(sizeof(char *) * 3);
-	octal_str = ft_itoa_base(mode, 8);
-	if (!permission || !octal_str)
-		return (NULL);
-	ft_printf("%s}\n", octal_str);
-	while (index < 3)
-	{
-		index++;
-	}
-	free(octal_str);
-	return (NULL);
-}
+// 	index = 0;
+// 	index_1 = 0;
+// 	perm_val = 0;
+// 	tmp = NULL;
+// 	octal_str = NULL;
+// 	permission = NULL;
+// 	permission = (char **)malloc(sizeof(char *) * 3);
+// 	octal_str = ft_itoa_base(mode, 8);
+// 	if (!permission || !octal_str)
+// 		return (NULL);
+// 	tmp = ft_strdup(&octal_str[ft_strlen(octal_str) - 3]);
+// 	ft_printf("%s}\n", tmp);
+// 	while (index < 3)
+// 	{
+// 		permission[index] = (char *)malloc(3);
+// 		if (!permission[index])
+// 			return (NULL);
+// 		ft_strcpy(permission[index], "rwx");
+// 		perm_val = tmp[index] - '0';
+// 		pow_of = 4;
+// 		while (permission[index][index_1] != '\0')
+// 		{
+// 			perm_val -= pow_of;
+// 			if (!perm_val)
+// 				permission[index][index_1] = '-';
+// 			pow_of -= 2;
+// 			index_1++;
+// 		}
+// 		index++;
+// 	}
+// 	free(tmp);
+// 	free(octal_str);
+// 	return (NULL);
+// }
+
+// static void permission_parse(int perm_int, int loop_int, char *bitfield)
+// {
+// 	int	nbr
+	
+// 	if (perm_int > 0)
+// 	{
+// 		if ((perm_int - loop_int) <= 0)
+// 			*bitfield = '-';
+// 		permission_parse()
+// 	}
+// }
 
 char	*permission_str(mode_t mode)
 {
-	char **testing;
+	int		index;
+	int		pow_of;
+	int		index_1;
+	int		perm_val;
+	char	*permission;
+	char	*octal_str;
+	char	*tmp;
+	
 
-	testing = permission_array(mode);
-	return (NULL);
+	index = 0;
+	perm_val = 0;
+	tmp = NULL;
+	permission = ft_strdup("rwxrwxrwx");
+	octal_str = ft_itoa_base(mode, 8);
+	if (!permission || !octal_str)
+		return (NULL);
+	tmp = ft_strdup(&octal_str[ft_strlen(octal_str) - 3]);
+	// ft_printf("%s}\n", tmp);
+	while (index < 9)
+	{
+		perm_val = *tmp - '0';
+		pow_of = 4;
+		index_1 = 0;
+		while (index_1 < 3)
+		{
+			if (perm_val / pow_of != 0)
+			{	
+				perm_val -= pow_of;
+			}
+			else
+				permission[index + index_1] = '-';
+			
+			pow_of -= pow_of / 2;
+			index_1++;
+		}
+		// ft_printf("\n");
+		tmp++;
+		index += 3;
+	}
+	// ft_printf("%s}\n", permission);
+	free(tmp-3);
+	free(octal_str);
+	return (permission);
 }
+
+// char	*permission_str(mode_t mode)
+// {
+// 	char **testing;
+
+// 	testing = permission_array(mode);
+// 	return (NULL);
+// }
