4fbbcb171d314a3a2e1bcd4fd3e4799f69aad10c
MrMikey695 <mikeedubas@hotmail.com>
2022-08-06 13:28:47 +0300

    implmented bitwise operations for multiple arguments

--- srcs/multiple_args_util.c
+++ srcs/multiple_args_util.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   multiple_args_util.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarutel <mbarutel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 13:47:07 by mbarutel          #+#    #+#             */
-/*   Updated: 2022/08/04 16:00:16 by mbarutel         ###   ########.fr       */
+/*   Updated: 2022/08/06 13:28:07 by mbarutel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,21 @@ void	args_del(t_args *args, int index)
 	free(args);
 }
 
-static void	args_swap(int i, int y, t_args *args)
+static void	args_swap(t_args *args_1, t_args *args_2)
 {
-	t_args	ptr;
+	uintptr_t	ptr_1;
+	uintptr_t	ptr_2;
 
-	ptr = args[i];
-	args[i] = args[y];
-	args[y] = ptr;
+	ptr_1 = (uintptr_t)args_1->file_name;
+	ptr_2 = (uintptr_t)args_2->file_name;
+	ptr_1 = ptr_1 ^ ptr_2;
+	ptr_2 = ptr_1 ^ ptr_2;
+	ptr_1 = ptr_1 ^ ptr_2;
+	args_1->file_name = (char *)ptr_1;
+	args_2->file_name = (char *)ptr_2;
+	args_1->modi_date = args_1->modi_date ^ args_2->modi_date;
+	args_2->modi_date = args_1->modi_date ^ args_2->modi_date;
+	args_1->modi_date = args_1->modi_date ^ args_2->modi_date;
 }
 
 void	args_lexi_swap(int i, int y, t_args *args, t_opts *opt)
@@ -36,11 +44,11 @@ void	args_lexi_swap(int i, int y, t_args *args, t_opts *opt)
 	if (opt->rev)
 	{
 		if (ft_strcmp(args[i].file_name, args[y].file_name) < 0)
-			args_swap(i, y, args);
+			args_swap(&args[i], &args[y]);
 	}
 	else
 		if (ft_strcmp(args[i].file_name, args[y].file_name) > 0)
-			args_swap(i, y, args);
+			args_swap(&args[i], &args[y]);
 }
 
 void	args_date_swap(int i, int y, t_args *args, t_opts *opt)
@@ -48,12 +56,12 @@ void	args_date_swap(int i, int y, t_args *args, t_opts *opt)
 	if (opt->rev)
 	{
 		if (args[i].modi_date > args[y].modi_date)
-			args_swap(i, y, args);
+			args_swap(&args[i], &args[y]);
 	}
 	else
 		if (args[i].modi_date < args[y].modi_date)
-			args_swap(i, y, args);
-	if (args[i].modi_date == args[y].modi_date)
+			args_swap(&args[i], &args[y]);
+	if ((args[i].modi_date ^ args[y].modi_date) == 0)
 		args_lexi_swap(i, y, args, opt);
 }
 
